52868c6fb5dac5d9c1bd5438dacef2b02e41cefe
lepton <tonihirvonen00@gmail.com>
2024-05-31 21:31:48 +0300

    Config

---
+++ lua/custom/plugins/debugger.lua
@@ -0,0 +1,39 @@
+local plugins = {
+  -- Debugger
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'mfussenegger/nvim-dap',
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+    },
+    opts = {
+      handlers = {},
+    },
+  },
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      dapui.setup()
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
+
+      vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, {})
+      vim.keymap.set('n', '<leader>dr', dap.continue, {})
+    end,
+  },
+}
+
+return plugins
