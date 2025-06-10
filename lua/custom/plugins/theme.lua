d151aba66914db12cbeb21e60c296eae7b4eb1a4
lepton <tonihirvonen00@gmail.com>
2025-06-10 22:22:33 +0300

    theme

---
+++ lua/custom/plugins/theme.lua
@@ -0,0 +1,10 @@
+return {
+  'EdenEast/nightfox.nvim',
+  'catppuccin/nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- vim.cmd.colorscheme 'catppuccin'
+    vim.cmd.colorscheme 'tokyonight-night'
+  end,
+}
