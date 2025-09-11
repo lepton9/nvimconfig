042647c390541c07c1419daa71e1e6320321cbfc
lepton <tonihirvonen00@gmail.com>
2025-09-11 04:49:30 +0300

    elixir syntax hl

--- init.lua
+++ init.lua
@@ -674,11 +674,11 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'elixir' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
-        enable = false, -- Was true but not working on c files
+        enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
